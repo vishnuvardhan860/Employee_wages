35bbb924b5f17b27736b75270bcff4ff01bfe4a9
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-07-17 13:12:46 +0530

    Added:Wages_For_Month

---
+++ EmployeeBuilderUC5.java
@@ -0,0 +1,31 @@
+package com.employeebuilderuc1;
+
+public class EmployeeBuilderUC5 {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
+	
+	public static void main(String[] args) {
+		   //VARAIBLE
+	       int empHrs = 0, empWage = 0, totalEmpWage = 0;
+	       //computation
+	       for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+	       int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+	       switch (empCheck) {
+		      case IS_PART_TIME:
+		          empHrs = 4;
+		          break;
+		      case IS_FULL_TIME:
+		          empHrs = 8;
+		          break;
+		      default:
+		          empHrs = 0;
+	   }
+	    empWage = empHrs * EMP_RATE_PER_HOUR;
+	    totalEmpWage += empWage;
+	    System.out.println("Emp Wage: " + empWage);
+	 }
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+ }
+}
