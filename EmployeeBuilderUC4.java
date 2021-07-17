4c812076b98bd601b094070d9b4dfd59832e60f5
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-07-17 13:10:39 +0530

    Added:Using_Switching_Case_Statement

---
+++ EmployeeBuilderUC4.java
@@ -0,0 +1,27 @@
+package com.employeebuilderuc1;
+
+public class EmployeeBuilderUC4 {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	
+	public static void main(String[] args) {
+		      //VARAIBLE
+		       int empHrs = 0;
+		       int empWage = 0;
+		        //COMPUTATION
+		        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		        switch (empCheck) {
+		        case IS_PART_TIME:
+		        	empHrs = 4;
+		        	break;
+		        case IS_FULL_TIME:
+		        	empHrs = 8;
+		        	break;
+		        default:
+		             empHrs = 0;
+		        empWage = empHrs * EMP_RATE_PER_HOUR;
+		        System.out.println("Emp wage: " + empWage);
+			}
+}
+}
